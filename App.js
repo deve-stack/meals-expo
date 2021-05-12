0f539529e4ade2247ed047a5dfb8c01678d739b7
dev stack <gauravsingh@dcubetechnologies.com>
2021-05-12 12:47:50 +0530

    authentication part done

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import firebase from "firebase/app";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -21,19 +23,33 @@ export default function App() {
     Lato_400Regular,
   });
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCA8a99Y2qh9JhWY0CAcFl8AfJUpledbjk",
+    authDomain: "mealstogo-2eceb.firebaseapp.com",
+    projectId: "mealstogo-2eceb",
+    storageBucket: "mealstogo-2eceb.appspot.com",
+    messagingSenderId: "154012390885",
+    appId: "1:154012390885:web:7eac1f833e51e345458b0c",
+    measurementId: "G-H9K2QZ2VVM",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </>
