3cc29947777f682d3c6265f081b7bc8e92cb1c6e
dev stack <gauravsingh@dcubetechnologies.com>
2021-05-14 17:06:34 +0530

    final commit app finished loader bug fix

--- src/features/restaurants/screens/restaurants_screen.js
+++ src/features/restaurants/screens/restaurants_screen.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Colors, ActivityIndicator, Searchbar } from "react-native-paper";
-import { RestaurantInfo } from "../componets/restaurant-info-card";
-import styled from "styled-components";
-import { SafeArea } from "../../../components/utility/safe-area";
-import { useContext } from "react";
-import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
-import { Spacer } from "../../../components/spacer_component";
-import { Search } from "../componets/search.component";
-import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
-import { useState } from "react/cjs/react.development";
-import { FavouritesContext } from "../../../services/favourites/favourites.context";
-import { RestaurantList } from "../componets/restaurant-list.styles";
+import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
+
 import { FadeInView } from "../../../components/animations/fade.animation";
-import { RestaurantCard } from "../componets/restaurant-info-card-style";
+import { SafeArea } from "../../../components/utility/safe-area";
+import { Spacer } from "../../../components/spacer_component";
+import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 
-const Loading = styled(ActivityIndicator)``;
+import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
+import { Search } from "../componets/search.component";
+import { RestaurantInfo } from "../componets/restaurant-info-card";
+
+import { RestaurantList } from "../componets/restaurant-list.styles";
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
 const LoadingContainer = styled.View`
   position: absolute;
   top: 50%;
@@ -25,8 +27,6 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = ({ navigation }) => {
   const { isLoading, restaurants } = useContext(RestaurantsContext);
-  const [searchQuery, setSearchQuery] = React.useState("");
-  const onChangeSearch = (query) => setSearchQuery(query);
   const { favourites } = useContext(FavouritesContext);
   const [isToggled, setIsToggled] = useState(false);
 
@@ -34,12 +34,7 @@ export const RestaurantsScreen = ({ navigation }) => {
     <SafeArea>
       {isLoading && (
         <LoadingContainer>
-          <Loading
-            size={50}
-            animating={true}
-            color={Colors.blue300}
-            style={{ marginLeft: -25 }}
-          />
+          <Loading size={50} animating={true} color={Colors.blue300} />
         </LoadingContainer>
       )}
       <Search
@@ -52,9 +47,9 @@ export const RestaurantsScreen = ({ navigation }) => {
           onNavigate={navigation.navigate}
         />
       )}
+
       <RestaurantList
         data={restaurants}
-        keyExtractor={(item) => item.name}
         renderItem={({ item }) => {
           return (
             <TouchableOpacity
@@ -72,6 +67,7 @@ export const RestaurantsScreen = ({ navigation }) => {
             </TouchableOpacity>
           );
         }}
+        keyExtractor={(item) => item.name}
       />
     </SafeArea>
   );
